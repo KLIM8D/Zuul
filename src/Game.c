c71836fc52768f11c8e8f214bda1b88a9e84e8c6
Morten Klim Sorensen <mail@kl1m.dk>
2013-09-16 18:16:18 +0200

    c files added

--- src/Game.c
+++ src/Game.c
@@ -1,6 +1,19 @@
 #include "include/Game.h"
+#include "include/Item.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
+    extern item *itemp;
+
+    itemp->name = "Best item";
+    itemp->type = "Axe";
+    itemp->rareness = "Epic";
+    itemp->damage = 100;
+
+
+    printf("%s\n", getItemDesc());
+
     return 0;
 }
--- src/Item.c
+++ src/Item.c
@@ -1,15 +1,16 @@
 #include "include/Item.h"
 #include <stdio.h>
 
-static struct item *_item;
+/*static struct item _item;*/
+static struct item _item;
+struct item *itemp = &_item;
+
 char *getItemDesc(void)
 {
     char description[1000];
     char *p = description;
-    if(_item != NULL)
-    {
-        sprintf(description, "Name: %s\nType: %s\nRare: %s\nDamage: %ld", _item->name, _item->type,_item->rareness,_item->damage);
-    }
+        sprintf(description, "Name: %s\nType: %s\nRare: %s\nDamage: %ld", itemp->name, itemp->type, itemp->rareness, itemp->damage);
 
     return p;
 }
+
